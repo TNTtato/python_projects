b6fa76a24c7ca5d0667194188b6dff5d1b3e0745
gustavo <penarandagustavo5@gmail.com>
2022-09-21 11:25:22 -0500

    def linked list methods

---
+++ data_structures/linked_list/linked_list.py
@@ -0,0 +1,31 @@
+from node import Node
+
+class LinkedList:
+  def __init__(self):
+    self.size = 0
+    self.head = None
+    self.tail = None
+  
+  def append_node(self, value): #inserts values to the end of the list
+    pass
+
+  def unshift(self, value): #inserts values to the start of the list
+    pass
+
+  def value_at(self, index):
+    pass
+
+  def find(self, value): #return index
+    pass
+
+  def include(self, value):
+    pass
+
+  def insert_at(self, value, index):
+    pass
+
+  def delete_at(self, index):
+    pass
+
+  def each_node(self):
+    pass
