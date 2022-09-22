dae7121f001632ef16e8eca7cc1e5355dbdfbe97
gustavo <penarandagustavo5@gmail.com>
2022-09-22 14:42:11 -0500

    fix checkout hour

--- open_bootcamp/ejercicio9.py
+++ open_bootcamp/ejercicio9.py
@@ -6,6 +6,6 @@ current_minute = str(time.localtime().tm_min)
 current_second = str(time.localtime().tm_sec)
 
 format_h = "%H:%M:%S"
-salida = d.strptime("17:00:00", format_h)
+salida = d.strptime("19:00:00", format_h)
 actual = d.strptime(f'{current_hour}:{current_minute}:{current_second}', format_h)
 print("Time to go home!!") if actual >= salida else print(f'Aun queda {salida - actual} para salir')
