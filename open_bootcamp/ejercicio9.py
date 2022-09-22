9fff4c7a469e6a09d100f6e386e29f333f142d28
gustavo <penarandagustavo5@gmail.com>
2022-09-22 14:39:38 -0500

    ejercicio 9

---
+++ open_bootcamp/ejercicio9.py
@@ -0,0 +1,11 @@
+import time
+from datetime import time as t, datetime as d
+
+current_hour = str(time.localtime().tm_hour)
+current_minute = str(time.localtime().tm_min)
+current_second = str(time.localtime().tm_sec)
+
+format_h = "%H:%M:%S"
+salida = d.strptime("17:00:00", format_h)
+actual = d.strptime(f'{current_hour}:{current_minute}:{current_second}', format_h)
+print("Time to go home!!") if actual >= salida else print(f'Aun queda {salida - actual} para salir')
