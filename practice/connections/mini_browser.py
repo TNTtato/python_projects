37ff4e6d6b26c60921f996c832f38fcb95b37996
gustavo <penarandagustavo5@gmail.com>
2022-10-17 19:14:01 -0500

    simple browser using sockets

---
+++ practice/connections/mini_browser.py
@@ -0,0 +1,14 @@
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect(('localhost', 9000))
+cmd = 'GET http://localhost:9000 HTTP/1.1\r\n\r\n'.encode()
+mysocket.send(cmd)
+
+while True:
+  data = mysocket.recv(512)
+  if len(data) < 1:
+    break
+  print(data.decode(), end='')
+
+mysocket.close()
